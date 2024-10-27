19ee3e0699241d4a19019c7d9e3867958924cc62
kev-techi <kevin@techi.be>
2024-10-27 10:40:47 +0100

    export transaction status

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./config/index.ts";
 export * from "./constants/networks.ts";
 export * from "./profiles/index.ts";
+export * from "./transactions/index.ts";
---
+++ src/transactions/index.ts
@@ -0,0 +1,1 @@
+export type TransactionStatus = "sending" | "pending" | "success" | "fail";
