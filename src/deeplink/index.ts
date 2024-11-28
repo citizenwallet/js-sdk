7dbba54235fed8c9f5ff26f5b3df534a7339ff33
kev-techi <kevin@techi.be>
2024-11-28 14:27:46 +0100

    generate receive link

--- src/deeplink/index.ts
+++ src/deeplink/index.ts
@@ -1,6 +1,6 @@
 import { compress } from "../utils/gzip";
 
-export const generateReceiveLink = (
+export const generateLegacyReceiveLink = (
   baseUrl: string,
   account: string,
   alias: string,
@@ -19,4 +19,23 @@ export const generateReceiveLink = (
   const compressedParams = compress(receiveParams);
 
   return `${baseUrl}/#/?alias=${alias}&receiveParams=${compressedParams}`;
+};
+
+export const generateReceiveLink = (
+  baseUrl: string,
+  account: string,
+  alias: string,
+  amount?: string,
+  description?: string
+) => {
+  let url = `${baseUrl}/?sendto=${account}@${alias}`;
+  if (amount) {
+    url += `&amount=${amount}`;
+  }
+
+  if (description) {
+    url += `&description=${encodeURIComponent(description)}`;
+  }
+
+  return url;
 };
