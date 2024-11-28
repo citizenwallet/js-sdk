149af98615fd90006b1592d5cc5aed1d9d19a514
kev-techi <kevin@techi.be>
2024-11-28 14:49:32 +0100

    fix for compile

--- src/deeplink/index.ts
+++ src/deeplink/index.ts
@@ -34,7 +34,7 @@ export const generateReceiveLink = (
   }
 
   if (description) {
-    url += `&description=${description}`;
+    url += `&description=${encodeURIComponent(description)}`;
   }
 
   return url;
