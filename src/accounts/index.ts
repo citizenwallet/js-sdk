954732680a1176791defcde8c69516f1417d9372
kev-techi <kevin@techi.be>
2024-11-28 12:28:26 +0100

    account ownership verification

--- src/accounts/index.ts
+++ src/accounts/index.ts
@@ -1,6 +1,9 @@
 import accountFactoryAbi from "../abi/AccountFactory.abi.json" with { type: "json" };
+import erc20Abi from "../abi/ERC20.abi.json" with { type: "json" };
+import accountAbi from "../abi/Account.abi.json" with { type: "json" };
+import safeAccountAbi from "../abi/Safe.abi.json" with { type: "json" };
 import { type CommunityConfig } from "../config";
-import { JsonRpcProvider, Contract } from "ethers";
+import { JsonRpcProvider, Contract, verifyMessage, hashMessage} from "ethers";
 
 export const getAccountAddress = async (
   config: CommunityConfig,
@@ -28,3 +31,84 @@ export const getAccountAddress = async (
         return null;
     }
 };
+
+export const getAccountBalance = async (
+  config: CommunityConfig,
+  address: string
+): Promise<bigint | null> => {
+  const rpc = new JsonRpcProvider(config.primaryRPCUrl);
+  const contract = new Contract(
+    config.primaryToken.address,
+    erc20Abi,
+    rpc
+  );
+
+  try {
+    const balance = await contract.getFunction("balanceOf")(address);
+
+    return balance;
+  } catch (error) {
+    console.error("Error fetching account balance:", error);
+
+    return null;
+  }
+};
+
+export const verifyAccountOwnership = async (
+  config: CommunityConfig,
+  accountAddress: string,
+  message: string,
+  signature: string,
+) => {
+  const recoveredAddress = verifyMessage(message, signature);
+  if (recoveredAddress.toLowerCase() === accountAddress.toLowerCase()) {
+    return true;
+  }
+
+  try {
+    const rpc = new JsonRpcProvider(config.primaryRPCUrl);
+  const contract = new Contract(
+    accountAddress,
+    accountAbi,
+    rpc
+  );
+
+    // Check if isValidSignature is implemented by calling it
+    try {
+      const hash = hashMessage(message);
+      const magicValue = await contract.getFunction("isValidSignature")(
+        hash,
+        signature
+      );
+
+      if (magicValue !== "0x1626ba7e") {
+        return false;
+      }
+    } catch (error) {
+      console.warn(error);
+      // Function is not implemented
+      console.warn("isValidSignature is not implemented on this contract");
+      
+
+      const owner = await contract.getFunction("owner")();
+
+      if (owner.toLowerCase() !== accountAddress.toLowerCase()) {
+        return false;
+      }
+    }
+
+    const safeContract = new Contract(
+      accountAddress,
+      safeAccountAbi,
+      rpc
+    );
+
+    const isOwner = await safeContract.getFunction("isOwner")(recoveredAddress);
+
+    return isOwner;
+  } catch (error) {
+    console.error("Error verifying account ownership:", error);
+  }
+
+  return false;
+}
