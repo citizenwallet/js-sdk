ae6cd573078fb31dd4d73aa1e5ba20e6e5653024
sajee_techi <sajeewa@techi.be>
2025-04-30 15:57:55 +0530

    undo commit change

--- src/calldata/index.ts
+++ src/calldata/index.ts
@@ -11,9 +11,6 @@ export const tokenTransferSingleEventTopic = id(
   "TransferSingle(address,address,address,uint256,uint256)"
 );
 
-export const roleGrantedEventTopic = id("RoleGranted(bytes32,address,address)");
-export const roleRevokedEventTopic = id("RoleRevoked(bytes32,address,address)");
-
 export const tokenTransferCallData = (
   to: string,
   value: bigint
